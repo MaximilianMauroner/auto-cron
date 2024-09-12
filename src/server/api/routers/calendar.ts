51137553fd064f55d83030a0142865b0d4cc7d5b
Maximilian Mauroner <github@relay.mauroner.eu>
2024-09-12 19:49:40 +0200

    init 2

---
+++ src/server/api/routers/calendar.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { db } from "~/server/db";
+import { posts } from "~/server/db/schema";
+import { CalendarList } from "~/utils/types";
+
+export const calendarRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  getSecretMessage: protectedProcedure.query(async ({ ctx }) => {
+    const acc = await db.query.accounts.findFirst({
+      where: (accounts, { eq }) => eq(accounts.userId, ctx.session.user.id),
+    });
+    if (!acc) {
+      // todo
+      return null;
+    }
+    const calendars = (await fetch(
+      "https://www.googleapis.com/calendar/v3/users/me/calendarList",
+      {
+        headers: {
+          Authorization: `Bearer ${acc?.access_token}`,
+        },
+      },
+    ).then((res) => res.json())) as CalendarList;
+    return calendars;
+  }),
+});
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
+import GooogleProvider from "next-auth/providers/google";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -59,9 +59,15 @@ export const authOptions: NextAuthOptions = {
     verificationTokensTable: verificationTokens,
   }) as Adapter,
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GooogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/calendar.events.owned https://www.googleapis.com/auth/calendar",
+        },
+      },
     }),
     /**
      * ...add more providers here.
