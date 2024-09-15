097b5a3ccff19f3101e125e6d412271ed0cef519
Maximilian Mauroner <github@relay.mauroner.eu>
2024-09-15 16:40:39 +0200

    continue working on cal  logic

--- src/app/_components/calendar.tsx
+++ src/app/_components/calendar.tsx
@@ -13,49 +13,53 @@ type Event = {
 };
 
 import moment, { type Moment } from "moment";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Calendar() {
-  const [data, setData] = useState<Day[]>([]);
-  const date = moment(new Date());
+  const [data, setData] = useState<Day[]>([
+    {
+      date: moment("2024-09-09"),
+      events: [
+        {
+          startTime: moment("2024-09-09T08:00"),
+          endTime: moment("2024-09-09T12:00"),
+          hexColor: "#14b8a6",
+          name: "Event 1",
+        },
+        {
+          startTime: moment("2024-09-09T10:00"),
+          endTime: moment("2024-09-09T11:00"),
+          hexColor: "#14b8a6",
+          name: "Event 2",
+        },
+        {
+          startTime: moment("2024-09-09T11:00"),
+          endTime: moment("2024-09-09T14:00"),
+          hexColor: "#14b8a6",
+          name: "Event 3",
+        },
+        {
+          startTime: moment("2024-09-09T16:00"),
+          endTime: moment("2024-09-09T18:00"),
+          hexColor: "#14b8a6",
+          name: "Event 4",
+        },
+        {
+          startTime: moment("2024-09-09T10:30"),
+          endTime: moment("2024-09-09T11:30"),
+          hexColor: "#14b8a6",
+          name: "Event 4",
+        },
+      ],
+    },
+    { date: moment("2024-09-10"), events: [] },
+    { date: moment("2024-09-11"), events: [] },
+    { date: moment("2024-09-12"), events: [] },
+    { date: moment("2024-09-13"), events: [] },
+    { date: moment("2024-09-14"), events: [] },
+    { date: moment("2024-09-15"), events: [] },
+  ]);
 
-  const val = (date.weekday() + 6) % 7;
-
-  useEffect(() => {
-    prepareData();
-  }, []);
-
-  function prepareData() {
-    const tmp: Day[] = [];
-    for (let i = 0; i < 7; i++) {
-      if (i < val) {
-        tmp.push({ date: date.clone().subtract(val - i, "days"), events: [] });
-      } else {
-        tmp.push({ date: date.clone().add(i - val, "days"), events: [] });
-      }
-      let counter = i;
-      for (let j = 0; counter >= 0; ) {
-        if (Math.random() < 1 / 24) {
-          const row = tmp[i];
-          if (row) {
-            const startTime = row.date.clone().hour(j).second(0).minute(0);
-            const endTime = startTime
-              ?.clone()
-              .add(j + Math.random() * 5 + 1, "hours");
-            row.events.push({
-              name: "Event " + j,
-              startTime: startTime,
-              endTime: endTime,
-              hexColor: "#" + Math.floor(Math.random() * 16777215).toString(16),
-            });
-            counter--;
-          }
-        }
-        j = (j + 1) % 24;
-      }
-    }
-    setData(tmp);
-  }
   return (
     <>
       <div className="flex w-full flex-row">
@@ -99,7 +103,7 @@ export default function Calendar() {
 
 function DisplayDayEvents({ day }: { day: Day }) {
   const events = day.events;
-  const overlapMap = new Map<number, number>();
+  const overlaping = new Map<number, { overlaps: number; events: Event[] }>();
 
   for (let i = 0; i < events.length; i++) {
     for (let j = i + 1; j < events.length; j++) {
@@ -110,40 +114,74 @@ function DisplayDayEvents({ day }: { day: Day }) {
         continue;
       }
 
-      const startA = eventA.startTime.hour();
-      const endA = eventA.endTime.hour();
-      const startB = eventB.startTime.hour();
-      const endB = eventB.endTime.hour();
+      const startA = eventA.startTime;
+      const endA = eventA.endTime;
+      const startB = eventB.startTime;
+      const endB = eventB.endTime;
+      // if a starts before b && b ends before a
+      if (startA.isBefore(startB) && endA.isAfter(startB)) {
+        const getMinHr = startA.hour() * 60 + startA.minute();
 
-      if (startA < endB && startB < endA) {
-        // Calculate the overlapping range
-        const overlapStart = Math.max(startA, startB);
-        const overlapEnd = Math.min(endA, endB);
-
-        for (let hour = overlapStart; hour < overlapEnd; hour++) {
-          overlapMap.set(hour, (overlapMap.get(hour) ?? 0) + 1);
-        }
+        const getEndHr = endA.hour() * 60 + endA.minute();
       }
     }
   }
-  console.log(overlapMap, day.date.format("YYYY-MM-DD"));
+
+  console.log(overlaping);
+
+  function timeFix(hour: number, minute: number) {
+    let time = "";
+    if (hour < 10) {
+      time += "0" + hour;
+    } else {
+      time += hour;
+    }
+    time += ":";
+    if (minute < 10) {
+      time += "0" + minute;
+    } else {
+      time += minute;
+    }
+    return time;
+  }
   return (
     <>
       {events.map((event, i) => {
+        const startH = event.startTime.hour();
+        const startM = event.startTime.minute();
+        const endH = event.endTime.hour();
+        const endM = event.endTime.minute();
+
+        const width = "100%";
+        const zIndex = 0;
+        const left = "0%";
+
+        // if there is one event -> full with
+        // if there is complete overlap -> 50/50
+        // if there is partial overlap -> 100 on the longer and 50% on the smaller task, smaller task to the right
+
+        const startPercentage = ((startH + startM / 60) / 24) * 100;
+        const endPercentage = ((endH + endM / 60) / 24) * 100;
+
         return (
           <div
             key={i}
-            className="absolute h-16 w-full text-white"
+            className="absolute mb-1 mr-1 h-16 cursor-pointer touch-manipulation overflow-hidden rounded-lg border text-white"
             style={{
-              top: `${(+event.startTime.hour() / 24) * 100}%`,
-              height: `${((event.endTime.hour() - event.startTime.hour()) / 24) * 100}%`,
+              top: `${startPercentage}%`,
+              height: `${endPercentage - startPercentage}%`,
               backgroundColor: event.hexColor,
+              width: width,
+              zIndex: zIndex,
+              left: left,
             }}
           >
-            <span className="absolute left-0">
-              <b>{event.name}</b> <br />
-              {event.startTime.hour()}:00 - {event.endTime.hour()}:00
-            </span>
+            <div className="absolute left-0 grid">
+              <span className="font-bold">{event.name}</span>
+              <span className="text-xs">
+                {timeFix(startH, startM)} - {timeFix(endH, endM)} <br />
+              </span>
+            </div>
           </div>
         );
       })}
