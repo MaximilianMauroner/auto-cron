2d4a65e764ed37cbacb4d15f9c210a47b92b276c
Maximilian Mauroner <github@relay.mauroner.eu>
2024-09-16 18:18:18 +0200

    alsmost working,but need to modify how it is saved

--- src/app/_components/calendar.tsx
+++ src/app/_components/calendar.tsx
@@ -475,4 +475,119 @@ function DisplayEvent({
   );
 }
 
+function sortByRender(events: Event[]) {
+  events.sort(
+    ({ startTime: st1, endTime: et1 }, { startTime: st2, endTime: et2 }) => {
+      if (st1.isBefore(st2)) return -1;
+      if (st1.isAfter(st2)) return 1;
+      if (et1.isBefore(et2)) return -1;
+      if (et1.isAfter(et2)) return 1;
+      return 0;
+    },
+  );
+  const sortedByTime = events;
+  const sorted = [];
+  while (sortedByTime.length > 0) {
+    const event = sortedByTime.shift();
+    if (!event) continue;
+    sorted.push(event);
+
+    for (let i = 0; i < sortedByTime.length; i++) {
+      const test = sortedByTime[i];
+
+      // Still inside this event, look for next.
+      if (event?.endTime.isAfter(test?.startTime)) continue;
+
+      // We've found the first event of the next event group.
+      // If that event is not right next to our current event, we have to
+      // move it here.
+      if (i > 0) {
+        const event = sortedByTime.splice(i, 1)[0];
+        if (event) sorted.push(event);
+      }
+
+      // We've already found the next event group, so stop looking.
+      break;
+    }
+  }
+  return sorted;
+}
+
+function onSameRow(a: Event, b: Event, minimumStartDifference: number) {
+  return (
+    // Occupies the same start slot.
+    Math.abs(b.startTime.diff(a.startTime, "minutes")) <
+      minimumStartDifference ||
+    // A's start slot overlaps with b's end slot.
+    (b.startTime.isAfter(a.startTime) && b.startTime.isBefore(a.endTime))
+  );
+}
+
+export function getStyledEvents({
+  events,
+  minimumStartDifference,
+}: {
+  events: Event[];
+  minimumStartDifference: number;
+}) {
+  // Create proxy events and order them so that we don't have
+  // to fiddle with z-indexes.
+  const proxies = events.map((event) => structuredClone(event));
+  const eventsInRenderOrder = sortByRender(proxies);
+
+  // Group overlapping events, while keeping order.
+  // Every event is always one of: container, row or leaf.
+  // Containers can contain rows, and rows can contain leaves.
+  const containerEvents: Event[] = [];
+  for (const event of eventsInRenderOrder) {
+    // Check if this event can go into a container event.
+    const container = containerEvents.find(
+      (c) =>
+        c?.endTime.isAfter(event?.startTime) ||
+        Math.abs(event?.startTime.diff(c?.startTime, "minutes")) <
+          minimumStartDifference,
+    );
+
+    // Couldn't find a container — that means this event is a container.
+    if (!container) {
+      event.rows = [];
+      containerEvents.push(event);
+      continue;
+    }
+
+    // Found a container for the event.
+    event.container = container;
+
+    // Check if the event can be placed in an existing row.
+    // Start looking from behind.
+    let row = null;
+    for (let j = container.rows.length - 1; !row && j >= 0; j--) {
+      if (onSameRow(container.rows[j], event, minimumStartDifference)) {
+        row = container.rows[j];
+      }
+    }
+
+    if (row) {
+      // Found a row, so add it.
+      row.leaves.push(event);
+      event.row = row;
+    } else {
+      // Couldn't find a row – that means this event is a row.
+      event.leaves = [];
+      container.rows.push(event);
+    }
+  }
+
+  // Return the original events, along with their styles.
+  return eventsInRenderOrder.map((event) => ({
+    event: event.data,
+    style: {
+      top: event.top,
+      height: event.height,
+      width: event.width,
+      xOffset: Math.max(0, event.xOffset),
+    },
+  }));
+}
+
 // https://github.com/search?q=repo:jquense/react-big-calendar%20overlap.js&type=code
